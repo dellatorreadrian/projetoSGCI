24d68d4109d5bd73c6b5696fc731d8ad2ccf4106
Adrian Dellatorre <Adrian Dellatorre@DESKTOP-UFG2MEU>
2020-10-08 19:39:53 -0300

    Iniciando projeto.

---
+++ src/main/java/br/edu/iff/projetoSGCI/ProjetoSgciApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoSGCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSgciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSgciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iff/projetoSGCI/ProjetoSgciApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoSGCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSgciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
