06a2658988ebbf063f95f9862282d9534168f76d
Adrian Dellatorre <Adrian Dellatorre@DESKTOP-UFG2MEU>
2020-10-08 21:12:54 -0300

    Corrigindo problema de Build e Import

--- src/main/java/br/edu/iff/projetoSGCI/ProjetoSgciApplication.java
+++ src/main/java/br/edu/iff/projetoSGCI/ProjetoSgciApplication.java
@@ -3,6 +3,7 @@ package br.edu.iff.projetoSGCI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProjetoSgciApplication {
 
---
+++ src/main/java/br/edu/iff/projetoSGCI/model/Pessoa.java
@@ -0,0 +1,8 @@
+
+package br.edu.iff.projetoSGCI.model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable{
+    
+}
